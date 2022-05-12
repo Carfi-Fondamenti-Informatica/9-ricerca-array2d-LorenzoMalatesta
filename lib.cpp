81b072c7ec106d71301ea243786dc9f0e114f4a8
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-05-12 10:38:36 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,23 @@
+//
+// Created by Lorenzo Malatesta on 12/05/22.
+//
 
+#include "lib.h"
+
+int ricerca (char lista[10][20], char nome[20]) {
+    int risultato=-1;
+    for (int i=0; i<10; i++) {
+        int c=0;
+        for (int j=0; j<20; j++) {
+            if (lista[i][j]==nome[j]) {
+                c++;
+            } else {
+                break;
+            }
+        }
+        if (c==20) {
+            risultato=i;
+        }
+    }
+    return risultato;
+}
