f41471f39c784395dcc5de74b096a430cce4fccb
LorenzoMalatesta <100367512+LorenzoMalatesta@users.noreply.github.com>
2022-05-12 10:38:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char l[10][20], n[20];
+
+    for (int i=0; i<10; i++) {
+        for (int j=0; j<20; j++) {
+            l[i][j] = ' ';
+        }
+    }
+    for (int j=0; j<20; j++) {
+        n[j] = ' ';
+    }
+
+
+    for (int i=0; i<10; i++) {
+        cin >> l[i];
+    }
+    cin >> n;
+
+    int x = ricerca(l, n);
+    if (x>=0) {
+        cout << x;
+    } else {
+        cout << "non presente";
+    }
+
+    return 0;
 }
